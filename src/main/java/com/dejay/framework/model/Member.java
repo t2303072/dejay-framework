4dfcdd5e18710d9bd00d156d9ae80694adb27685
ijzone <ijzone@dejay.co.kr>
2023-06-09 11:31:26 +0900

    빌더패턴 생성자 기반으로 수정

--- src/main/java/com/dejay/framework/model/Member.java
+++ src/main/java/com/dejay/framework/model/Member.java
@@ -5,9 +5,7 @@ import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder
 @Getter
-@AllArgsConstructor
 @EqualsAndHashCode(of = {"id", "name"})
 public class Member {
     private Long id;
@@ -16,4 +14,11 @@ public class Member {
     private String email;
     @Builder.Default
     private List<String> list = new ArrayList<>();
+
+    @Builder
+    public Member(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
