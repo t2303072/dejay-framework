71cc439ae13fa39986e560842192995b1f985cae
ijzone <ijzone@dejay.co.kr>
2023-06-13 10:33:47 +0900

    인터셉터 설정

---
+++ src/main/java/com/dejay/framework/common/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.dejay.framework.common.config;
+
+import com.dejay.framework.common.interceptor.LoggerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private LoggerInterceptor loggerInterceptor;
+
+    @Autowired
+    public WebMvcConfig(LoggerInterceptor loggerInterceptor) {
+        this.loggerInterceptor = loggerInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor).addPathPatterns("/**/*");
+    }
+
+}
---
+++ src/main/java/com/dejay/framework/common/interceptor/LoggerInterceptor.java
@@ -0,0 +1,29 @@
+package com.dejay.framework.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+
+@Slf4j
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Request URI ==> {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("Handler ==> {}", handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
