8d27c13394e78a0be5e77fe4c90ec88d766c52d7
ijzone <ijzone@dejay.co.kr>
2023-06-14 18:26:36 +0900

    리다이렉션 샘플 추가

--- src/main/java/com/dejay/framework/IndexController.java
+++ src/main/java/com/dejay/framework/IndexController.java
@@ -1,26 +1,41 @@
 package com.dejay.framework;
 
 import com.dejay.framework.domain.Hello;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
+@RequestMapping("/index")
 @RestController
 public class IndexController {
 
     private static final String content = "%s";
     private final AtomicInteger counter = new AtomicInteger();
 
-    @GetMapping("/")
+    @GetMapping({"", "/"})
     public ResponseEntity index() {
         var hello = new Hello(counter.incrementAndGet(), String.format(content, "Dejay Framework"));
         HttpHeaders headers = new HttpHeaders();
         headers.add("headertest", "headervalue");
 
         return ResponseEntity.ok().headers(headers).body(hello);
+    }
+
+    @GetMapping("/redirect")
+    public ResponseEntity redirect(HttpServletRequest request) {
+        log.info("REDIRECT TEST");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/member"));
+        return new ResponseEntity(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
 }
