53133968c2cd5ec408af0f440dc73e31d3ebf24b
ijzone <ijzone@dejay.co.kr>
2023-07-03 18:25:22 +0900

    [공통] 암호화 모듈 추가

---
+++ src/main/java/com/dejay/framework/common/utils/CryptoUtil.java
@@ -0,0 +1,35 @@
+package com.dejay.framework.common.utils;
+
+import com.dejay.framework.common.enums.ExceptionCodeMsgEnum;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class CryptoUtil {
+
+    private PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    /**
+     * 비밀번호 암호화
+     * @param rawPassword {@link String}
+     * @return
+     */
+    public String encodePassword(String rawPassword) {
+        if(!StringUtils.hasText(rawPassword)) return new String();
+
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    /**
+     * 로그인 시, 동일 비밀번호 여부 확인
+     * @param rawPassword {@link String}
+     * @return
+     */
+    public boolean isPasswordMatches(String rawPassword, String encodedPassword) {
+        if(!StringUtils.hasText(rawPassword) || !StringUtils.hasText(encodedPassword)) throw new IllegalArgumentException(ExceptionCodeMsgEnum.PASSWORD_VALUE_NOT_PROVIDED.getMsg());
+        
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
--- src/main/java/com/dejay/framework/service/test/TestService.java
+++ src/main/java/com/dejay/framework/service/test/TestService.java
@@ -1,10 +1,16 @@
 package com.dejay.framework.service.test;
 
+import com.dejay.framework.common.utils.CryptoUtil;
 import com.dejay.framework.domain.common.Paging;
 import com.dejay.framework.vo.test.TestVO;
 import com.dejay.framework.vo.common.PagingVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,6 +20,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class TestService {
+
+    private final CryptoUtil cryptoUtil;
 
     public List<TestVO> getTest() {
         List list = new ArrayList<>();
@@ -33,5 +41,38 @@ public class TestService {
                 .totalCount(paging.getTotalCount())
                 .totalPage(paging.getTotalPage())
                 .build();
+    }
+
+    // TODO: IJ
+    public void passwordEncode() {
+        String rawPassword = "ijzone";
+        String encodedPassword = cryptoUtil.encodePassword(rawPassword);
+        log.info("encodedPassword: {}", encodedPassword);
+        boolean passwordMatches = cryptoUtil.isPasswordMatches(rawPassword, encodedPassword);
+        log.info("passwordMatches? {}", passwordMatches);
+        cryptoUtil.isPasswordMatches("", "");
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+//        String result = encoder.encode("ijzone");
+//        log.info("matches => {}", encoder.matches("ijzone", result));
+//
+//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        String ijzone = passwordEncoder.encode("ijzone");
+//        String ijzone2 = passwordEncoder.encode("ijzone");
+//        log.info("encoded password of ijzone: => {}", ijzone);
+//        log.info("BCryptPassword => {}", encoder.matches("ijzone", ijzone));
+//
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        UserDetails user = users.username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = users.username("admin")
+//                .password("password2")
+//                .roles("USER", "ADMIN")
+//                .build();
+//
+//        List authList = List.of(user.getAuthorities().toArray());
+//        log.info("admin: {}", admin.toString());
+//        log.info("user: {}", user.toString());
     }
 }
