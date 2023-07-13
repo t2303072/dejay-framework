8b97052f1ff9ac8bb94b11d0fd08cc5a3855e309
jokh <jokh1022@dejay.co.kr>
2023-07-13 18:03:56 +0900

    jenkins build & deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-sudo nohup java -jar -Dspring.profiles.active=dev /home/jenkins/jenkins_home/workspace/dejay_backend/build/libs/framework-0.0.1-SNAPSHOT.jar > &>/dev/null &
+sudo nohup java -jar -Dspring.profiles.active=dev /home/jenkins/jenkins_home/workspace/dejay_backend/build/libs/framework-0.0.1-SNAPSHOT.jar >> log.log 2>&1 &
