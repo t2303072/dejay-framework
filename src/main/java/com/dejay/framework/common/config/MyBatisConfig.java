1e8dcc4400d17cd7418c04eb550c2e7bb7ae0b5c
ijzone <ijzone@dejay.co.kr>
2023-07-14 13:35:28 +0900

    [공통] MyBatis 설정 변경 &  Interceptor 코드 추가(미적용)

---
+++ src/main/java/com/dejay/framework/common/config/MyBatisConfig.java
@@ -0,0 +1,49 @@
+package com.dejay.framework.common.config;
+
+import com.dejay.framework.common.interceptor.MyBatisInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.logging.slf4j.Slf4jImpl;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
+@MapperScan(value = "com.dejay.framework.mapper")
+@Configuration
+public class MyBatisConfig {
+
+    private final DataSource dataSource;
+
+    @Value("${mybatis.mapper-locations}")
+    private String mapperLocations;
+
+    @Value("${mybatis.type-aliases-package}")
+    private String typeAliasesPackage;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+//        sqlSessionFactoryBean.setPlugins(new MyBatisInterceptor()); TODO: IJ pause applying interceptor
+
+        Resource[] resources = new PathMatchingResourcePatternResolver().getResources(mapperLocations);
+        sqlSessionFactoryBean.setMapperLocations(resources);
+        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        org.apache.ibatis.session.Configuration configuration = sqlSessionFactory.getConfiguration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCallSettersOnNulls(true);
+        configuration.setJdbcTypeForNull(JdbcType.VARCHAR);
+
+        return sqlSessionFactory;
+    }
+}
