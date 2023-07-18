cbcc1f940b3f7a2eb6da4d89a037886eb76f61c3
jokh <jokh1022@dejay.co.kr>
2023-07-18 10:29:02 +0900

    Revert "jenkins build & deploy test"
    This reverts commit fec9d7a92629ec5a21e18589886e1a2a42e24a53.
    
    # Conflicts:
    #	start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,6 @@ fi
 
 echo "Deploy Project...." 
 
-nohup java -jar -Dspring.profiles.active=dev -Dproperties.jasypt.encryptor.password=dejay1234 /var/jenkins_home/workspace/dejay_backend/build/libs/framework-0.0.1-SNAPSHOT.jar >> /var/jenkins_home/workspace/dejay_backend/logs/framework.log 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev -Dproperties.jasypt.encryptor.password='dejay1234!@#$' /home/jenkins/jenkins_home/workspace/dejay_backend/build/libs/framework-0.0.1-SNAPSHOT.jar >> /home/jenkins/jenkins_home/workspace/dejay_backend/logs/framework.log 2>&1 &
 
 echo "Done"
