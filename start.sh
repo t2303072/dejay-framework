ce6151e0d88392f16fabd04dea626ea16fa8ef5c
jokh1022 <jokh1022@dejay.co.kr>
2023-07-27 13:15:54 +0900

    배포 쉘스크립트 수정
    배포 쉘스크립트 수정

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
-echo "PID Check..." 
+echo "PID Check..."
 
-CURRENT_PID=$(ps -ef | grep java | grep framework-0.0.1-SNAPSHOT* | awk '{print $2}') 
+CURRENT_PID=$(ps -ef | grep java | grep framework-1.*-RELEASE | awk '{print $2}')
 
-echo "Running PID: {$CURRENT_PID}" 
+echo "Running PID: {$CURRENT_PID}"
 
-if [ -z "$CURRENT_PID" ] ; then 
-	echo "Project is not running"
-else 
-	kill -9 $CURRENT_PID 
-	sleep 10 
-fi 
+if [ -z "$CURRENT_PID" ] ; then
+        echo "Project is not running"
+else
+        kill -9 $CURRENT_PID
+        sleep 10
+fi
 
-echo "Deploy Project...." 
+echo "Deploy Project...."
 
-nohup java -jar -Dspring.profiles.active=dev -Dproperties.jasypt.encryptor.password='dejay1234!@#$' /home/jenkins/jenkins_home/workspace/dejay_backend/build/libs/framework-0.0.1-SNAPSHOT.jar >> /home/jenkins/jenkins_home/workspace/dejay_backend/logs/framework.log 2>&1 &
+cd /home/jenkins/jenkins_home/workspace/dejay_backend
+
+JAR_FILE=$(find ./build/libs/ -type f -iname "framework-1.*-RELEASE.jar")
+#echo $JAR_FILE
+
+#nohup java -jar -Dspring.profiles.active=dev -Dproperties.jasypt.encryptor.password='dejay1234!@#$' ./build/libs/framework-1.*.*.RELEASE.jar >> ./logs/framework.log 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev -Dproperties.jasypt.encryptor.password='dejay1234!@#$' $JAR_FILE >> ./logs/framework.log 2>&1 &
 
 echo "Done"
