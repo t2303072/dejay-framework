397762bc52f0ab4c4b129d07b1ae9ddea4c97ef3
Seounggyun <hsk9839@dejay.co.kr>
2023-08-30 14:51:36 +0900

    [개발] (게시판) BoardVO 파일VO객체 import 삭제

--- src/main/java/com/dejay/framework/vo/board/BoardVO.java
+++ src/main/java/com/dejay/framework/vo/board/BoardVO.java
@@ -1,6 +1,5 @@
 package com.dejay.framework.vo.board;
 
-import com.dejay.framework.vo.file.FileVO;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
