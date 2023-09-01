f16c368a70678f603ee67ad5fcd76f3d53122ebd
ijzone <ijzone@dejay.co.kr>
2023-09-01 15:33:21 +0900

    [공통] (문서) request & response body fields 추가

--- src/test/java/com/dejay/framework/FrameworkApplicationTests.java
+++ src/test/java/com/dejay/framework/FrameworkApplicationTests.java
@@ -1,5 +1,6 @@
 package com.dejay.framework;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,18 +15,25 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
 import org.springframework.test.web.servlet.setup.MockMvcConfigurerAdapter;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
+@Transactional
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-@SpringBootTest(classes = FrameworkApplication.class)
 @ActiveProfiles("local")
+@SpringBootTest(classes = FrameworkApplication.class)
 public class FrameworkApplicationTests {
 
 	protected MockMvc mockMvc;
@@ -45,6 +53,49 @@ public class FrameworkApplicationTests {
 				.addFilter(proxyFilter)
 				.apply(documentationConfiguration(restDocumentation))
 				.build();
+	}
+
+	/**
+	 * Object -> JSON 문자열 변환
+	 * @param obj
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	protected String convertObjectToJsonString(Object obj) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	/**
+	 * JSON -> Map 변환
+	 * @param json {@link String}
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	protected HashMap convertJsonToMap(String json) throws JsonProcessingException {
+		return objectMapper.readValue(json, HashMap.class);
+	}
+
+	/**
+	 * JSON -> Map 변환
+	 * @param mvcResult {@link MvcResult}
+	 * @return
+	 * @throws JsonProcessingException
+	 * @throws UnsupportedEncodingException
+	 */
+	protected HashMap convertJsonToMap(MvcResult mvcResult) throws JsonProcessingException, UnsupportedEncodingException {
+		return objectMapper.readValue(mvcResult.getResponse().getContentAsString(), HashMap.class);
+	}
+
+	/**
+	 * Map -> Object 변환
+	 * @param responseBody
+	 * @param objKey
+	 * @param clazz
+	 * @return
+	 * @param <T>
+	 */
+	protected <T> T convertMapToObject(Map responseBody, String objKey, Class<T> clazz) {
+		return objectMapper.convertValue(((Map<Object, Object>) responseBody.get("data")).get(objKey), clazz);
 	}
 
 	@Test
--- src/test/java/com/dejay/framework/controller/login/LoginControllerTest.java
+++ src/test/java/com/dejay/framework/controller/login/LoginControllerTest.java
@@ -1,31 +1,105 @@
 package com.dejay.framework.controller.login;
 
 import com.dejay.framework.FrameworkApplicationTests;
+import com.dejay.framework.common.enums.MapKeyStringEnum;
 import com.dejay.framework.common.exception.CustomLoginException;
+import com.dejay.framework.domain.common.TokenObjectVO;
 import com.dejay.framework.domain.member.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.FieldDescriptor;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@Slf4j
 class LoginControllerTest extends FrameworkApplicationTests {
 
+    // Request Body Fields
+    FieldDescriptor[] requestFields = new FieldDescriptor[] {
+            fieldWithPath("userName").description("로그인 ID"),
+            fieldWithPath("password").description("비밀번호"),
+            fieldWithPath("authority").description("권한").optional()
+    };
+
+    // Succeeded Response Body Fields
+    FieldDescriptor[] successResponseFields = new FieldDescriptor[] {
+            fieldWithPath("resultStatus").description("응답 상태 객체"),
+            fieldWithPath("resultStatus.code").description("응답 코드"),
+            fieldWithPath("resultStatus.message").description("응답 메시지"),
+            fieldWithPath("resultStatus.specificMsg").description("상세 메시지"),
+            fieldWithPath("resultStatus.fieldErrors").description("필드 에러"),
+            fieldWithPath("data").description("응답 데이터"),
+            fieldWithPath("data.tokenObject").description("토큰"),
+            fieldWithPath("data.tokenObject.tableName").description("테이블"),
+            fieldWithPath("data.tokenObject.logId1").description("PK1"),
+            fieldWithPath("data.tokenObject.logId2").description("PK2"),
+            fieldWithPath("data.tokenObject.logType").description("처리 유형"),
+            fieldWithPath("data.tokenObject.logJson").description("처리 데이터"),
+            fieldWithPath("data.tokenObject.remark").description("비고"),
+            fieldWithPath("data.tokenObject.regId").description("등록 ID"),
+            fieldWithPath("data.tokenObject.key").description("발급 대상"),
+            fieldWithPath("data.tokenObject.accessToken").description("액세스 토큰"),
+            fieldWithPath("data.tokenObject.refreshToken").description("리프레시 토큰")
+    };
+
+    // Failed Response Body Fields with FieldErrors
+    FieldDescriptor[] failResponseFieldsWithFieldErrors = new FieldDescriptor[] {
+            fieldWithPath("code").description("응답 코드"),
+            fieldWithPath("message").description("응답 메시지"),
+            fieldWithPath("specificMsg").description("상세 메시지"),
+            fieldWithPath("fieldErrors[]").description("필드 에러"),
+            fieldWithPath("fieldErrors[].codes[]").description("필수 필드 에러 코드"),
+            fieldWithPath("fieldErrors[].arguments[].codes[]").description("필수 매개변수 필드 에러 코드"),
+            fieldWithPath("fieldErrors[].arguments[].arguments").description("매개변수"),
+            fieldWithPath("fieldErrors[].arguments[].defaultMessage").description("기본 에러 메시지"),
+            fieldWithPath("fieldErrors[].arguments[].code").description("매개변수 에러 코드"),
+            fieldWithPath("fieldErrors[].defaultMessage").description("기본 에러 메시지"),
+            fieldWithPath("fieldErrors[].objectName").description("매개변수 전달 객체 명"),
+            fieldWithPath("fieldErrors[].field").description("에러 필드"),
+            fieldWithPath("fieldErrors[].rejectedValue").description("반려 필드 값"),
+            fieldWithPath("fieldErrors[].bindingFailure").description("바인딩 실패 여부"),
+            fieldWithPath("fieldErrors[].code").description("에러 코드")
+    };
+
+
+    // Failed Response Body Fields
+    FieldDescriptor[] failResponseFields = new FieldDescriptor[] {
+            fieldWithPath("code").description("응답 코드"),
+            fieldWithPath("message").description("응답 메시지"),
+            fieldWithPath("specificMsg").description("상세 메시지"),
+            fieldWithPath("fieldErrors").description("필드 에러")
+    };
+
+//    @Rollback(false) // No rollback
     @Test
     void 로그인_성공() throws Exception {
         var target = LoginRequest.builder()
                 .userName("ijzone")
                 .password("12345")
                 .build();
-        String content = objectMapper.writeValueAsString(target);
+        String content = convertObjectToJsonString(target);
 
         this.mockMvc.perform(post("/login")
-                        .content(content)
+                        .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .content(content)
+                )
                 .andExpect(status().isOk())
-                .andDo(document("post-login-ok"));
+                .andExpect(res -> {
+                    TokenObjectVO tokenObjectVO = convertMapToObject(convertJsonToMap(res.getResponse().getContentAsString().toString()), MapKeyStringEnum.TOKEN_OBJECT.getKeyString(), TokenObjectVO.class);
+                    assertThat(tokenObjectVO.getKey()).isEqualTo("ijzone");
+                })
+                .andDo(document("post-login-ok"
+                        , requestFields(requestFields)
+                        , responseFields(successResponseFields))
+                );
     }
 
     @Test
@@ -33,15 +107,22 @@ class LoginControllerTest extends FrameworkApplicationTests {
         var target = LoginRequest.builder()
                 .password("12345")
                 .build();
-        String content = objectMapper.writeValueAsString(target);
+        String content = convertObjectToJsonString(target);
 
         this.mockMvc.perform(post("/login")
-                        .content(content)
+                        .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .content(content)
+                )
                 .andExpect(status().isBadRequest())
-                .andExpect(result -> result.getResolvedException().getClass().equals(CustomLoginException.class))
-                .andDo(document("post-no-login-id-fail"));
+                .andExpect(res -> res.getResolvedException().getClass().equals(CustomLoginException.class))
+                .andDo(document("post-no-login-id-fail"
+                        , requestFields(requestFields)
+                        , responseFields(failResponseFieldsWithFieldErrors))
+                )
+                .andDo(res -> {
+                    log.info("응답값 => {}", res.getResponse().getContentAsString());
+                });
     }
 
     @Test
@@ -53,11 +134,16 @@ class LoginControllerTest extends FrameworkApplicationTests {
         String content = objectMapper.writeValueAsString(target);
 
         this.mockMvc.perform(post("/login")
-                        .content(content)
+                        .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> result.getResolvedException().getClass().equals(CustomLoginException.class))
-                .andDo(document("post-login-wrong-password-fail"));
+                        .content(content)
+                )
+                .andExpect(status().isUnauthorized())
+                .andExpect(res -> res.getResolvedException().getClass().equals(CustomLoginException.class))
+                .andDo(document("post-login-wrong-password-fail"
+                        , requestFields(requestFields)
+                        , responseFields(failResponseFields))
+                );
     }
+
 }
