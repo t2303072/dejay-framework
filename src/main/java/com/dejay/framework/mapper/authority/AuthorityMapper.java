f3c398de84a62afa49286bcc40b12f2566fa152c
ijzone <ijzone@dejay.co.kr>
2023-09-04 16:53:08 +0900

    [공통] (매퍼) 파라미터 명시적 선언

--- src/main/java/com/dejay/framework/mapper/authority/AuthorityMapper.java
+++ src/main/java/com/dejay/framework/mapper/authority/AuthorityMapper.java
@@ -3,9 +3,10 @@ package com.dejay.framework.mapper.authority;
 import com.dejay.framework.mapper.common.GeneralMapper;
 import com.dejay.framework.vo.authority.MenuAuthorityVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AuthorityMapper extends GeneralMapper {
 
-    MenuAuthorityVO selectMenuAuthority(String memberId, String menuId);
+    MenuAuthorityVO selectMenuAuthority(@Param("memberId") String memberId, @Param("menuId") String menuId);
 }
