f7ab2c9ca4e8089d1d3b88d9cf51f9cdcf4fdf71
ijzone <ijzone@dejay.co.kr>
2023-09-07 10:04:10 +0900

    [공통] (UI) Spring form tag 추가

--- src/main/java/com/dejay/framework/jsp/JSPService.java
+++ src/main/java/com/dejay/framework/jsp/JSPService.java
@@ -3,6 +3,8 @@ package com.dejay.framework.jsp;
 import java.util.Collection;
 
 public interface JSPService {
+
+    SampleVO getSample();
     Collection<SampleVO> getSampleList();
 
     SampleVO addSample(SampleVO sampleVO);
