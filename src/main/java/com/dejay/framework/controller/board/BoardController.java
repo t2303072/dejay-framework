007c196dfdb653288379754c40e64c312590f7bf
ijzone <ijzone@dejay.co.kr>
2023-10-11 14:39:31 +0900

    [공통] (UI) 화면 UTF-8 설정

---
+++ src/main/java/com/dejay/framework/controller/board/BoardController.java
@@ -0,0 +1,24 @@
+package com.dejay.framework.controller.board;
+
+import com.dejay.framework.controller.common.ParentController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController extends ParentController {
+
+    @GetMapping({"", "/"})
+    public ModelAndView board(ModelAndView mv) {
+        mv.addObject("hello", "드제이");
+        mv.setViewName("board/list");
+
+        return mv;
+    }
+}
